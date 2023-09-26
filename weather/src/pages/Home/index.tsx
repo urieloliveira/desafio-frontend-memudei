c884b72ca54f7f5300d228872a613def6c88b1ac
Uildson S. dos Santos <wilder_shon@hotmail.com>
2023-09-26 18:07:39 -0300

    implementar layout responsivo da home

--- weather/src/pages/Home/index.tsx
+++ weather/src/pages/Home/index.tsx
@@ -1,35 +1,40 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faArrowDown,
+  faArrowUp,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
 
-import "./styles.css";
 import CapitaisMinMax from "components/CapitaisMinMax";
 
+import "./styles.css";
+
 const Home = () => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("mudou:" + event.target.value);
+  };
 
-  const handleChange  = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("mudou:" +event.target.value)
-  }
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("mudou: formulario" )
-  }
+  const handleSubmit = (event: React.MouseEvent<HTMLDivElement>) => {
+    console.log("mudou: formulario");
+  };
   return (
     <div className="home-container">
       <div className="home-search-content">
         <h1>Previsão do tempo</h1>
+
         <div className="form-container">
-          <form onSubmit={handleSubmit}>
+          <div className="input-container">
             <input
               type="text"
               className="search-input"
               placeholder="Insira aqui o nome da cidade"
               onChange={handleChange}
-            ></input>
-            <button type="submit" className="icon-button">
-              <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
-            </button>
-          </form>
+            />
+            <div className="icon" onClick={handleSubmit}>
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+          </div>
         </div>
       </div>
 
